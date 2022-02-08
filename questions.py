1fd3cc608671ae4098ec6a2a0500535d00070bd5
benlamb95 <benl1995@hotmail.co.uk>
2022-02-08 16:11:45 +0000

    Move questions to a seperate file to maintain readability

---
+++ questions.py
@@ -0,0 +1,101 @@
+question_prompt = [
+    "Who is the all-time top goalscorer?\n \
+        (a) Thierry Henry\n \
+        (b) Alan Shearer\n \
+        (c) Wayne Rooney\n",
+
+    "Who tops the all-time top Assists?\n \
+        (a) Ryan Giggs\n \
+        (b) Cesc Fabregas\n \
+        (c) Wayne Rooney\n",
+
+    "Who holds the record for most appearances?\n \
+        (a) Frank Lampard\n \
+        (b) Gareth Barry\n \
+        (c) Ryan Giggs\n",
+
+    "Who has won the competition the most times?\n \
+        (a) Manchester United\n \
+        (b) Arsenal\n \
+        (c) Chelsea\n",
+
+    "Who holds the record for most clean sheets?\n \
+        (a) David James\n \
+        (b) David Seaman\n \
+        (c) Petr Cech\n",
+
+    "Who holds the record for most losses?\n \
+        (a) West Ham\n \
+        (b) Newcastle\n \
+        (c) Everton\n",
+
+    "When was the League founded?\n \
+        (a) 1992\n \
+        (b) 1991\n \
+        (c) 1993\n",
+
+    "Derby holds the record for fewest wins in a season with how many?\n \
+        (a) 4\n \
+        (b) 3\n \
+        (c) 1\n",
+
+    "Which club holds the record for the highest single match attendance?\n \
+        (a) Tottenham\n \
+        (b) Manchester United\n \
+        (c) Arsenal\n",
+
+    "What was unique about the 2020-21 season?\n \
+        (a) There were more goals scored than any other season\n \
+        (b) Only season in which there were more away wins than home wins\n \
+        (c) No English managers in the top flight for the first time\n",
+
+    "Which manager has taken charge of the most clubs in the Premier League ?\n \
+        (a) Steve Bruce\n \
+        (b) Sam Allardyce\n \
+        (c) Harry Redknapp\n",
+
+    "Who is the highest scoring African?\n \
+        (a) Mohamed Salah\n \
+        (b) Didier Drogba\n \
+        (c) Yaya Toure\n",
+
+    "Who is the only player to have lost two games but scored hat-tricks?\n \
+        (a) Matt Le Tissier\n \
+        (b) Alan Shearer\n \
+        (c) Dwight Yorke\n",
+
+    "Which club plays at the London Stadium?\n \
+        (a) Chelsea\n \
+        (b) Brentford\n \
+        (c) West Ham\n",
+
+    "Which club only picked four non-British players when in the Prem?\n \
+        (a) Barnsley\n \
+        (b) Hull City\n \
+        (c) Oldham\n",
+
+    "Which club holds the record without getting a red card?\n \
+        (a) Liverpool\n \
+        (b) Burnley\n \
+        (c) Leicester\n",
+
+    "Who was the top goal scorer of the first ever Premier League season?\n \
+        (a) Teddy Sheringham\n \
+        (b) Les Ferdinand\n \
+        (c) Dean Holdsworth\n",
+
+    "The fastest goal scored in the prem is how many seconds?\n \
+        (a) 7.40 seconds\n \
+        (b) 8.02 seconds\n \
+        (c) 7.69 seconds\n",
+
+    "Who has scored the most headers?\n \
+        (a) Alan Shearer\n \
+        (b) Peter Crouch\n \
+        (c) Didier Drogba\n",
+
+    "Wat are Leiceter City also known as?\n \
+        (a) The Walkers\n \
+        (b) The Kings\n \
+        (c) The Foxes\n",
+]
